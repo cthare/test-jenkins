040de420156e2c1895716d0fac7aef9c162edb7f
cthare <colin@colinhare.com>
2022-05-18 18:12:55 -0400

    initial upload

---
+++ jenkins.ps1
@@ -0,0 +1,17 @@
+ function Test-Jenkins {
+
+	param(
+	     [Parameter()]
+	     [string]$testText,
+	 
+	     [Parameter()]
+	     [string]$pathForFile,
+
+	     [Parameter()]
+	     [string]$fileName
+	 )
+	 
+	 $testText | Out-File -FilePath ($pathForFile + $fileName)
+
+
+} 
